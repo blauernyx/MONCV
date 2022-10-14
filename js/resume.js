5fe4f2f6fb7488c0edf88277aa1e83345a4641c8
wissal romadi <wissalromadi@gmail.com>
2022-10-14 11:19:12 +0200

    description

---
+++ js/resume.js
@@ -0,0 +1,60 @@
+
+function details(id) {
+    var div = document.getElementById(id);
+    if (div.style.display === "none") {
+    div.style.display = "block";
+    fadeIn(div,3000);
+    //zoomtext(id); 
+    } 
+    else {
+    div.style.display = "none";
+    }
+}
+
+function alterner(id){
+    if(id=='stg1'){
+        details('stg1');
+        document.getElementById('stg2').style.display = 'none';
+        //zoomtext('stg1txt');
+    }
+    else {
+        details('stg2');
+        document.getElementById('stg1').style.display = 'none';
+        //zoomtext('stg2txt');
+    }    
+}
+
+
+function fadeIn(el, time) {
+    el.style.opacity = 0;
+    var last = +new Date();
+    var tick = function () {
+        el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+        last = +new Date();
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+            setTimeout(tick, 16);
+        }
+    };
+    tick();
+}
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+
+function zoomtext(id){
+    var textdiv = document.getElementById(id);
+    var fontSize = textdiv.style.fontSize;
+   
+    if (fontSize !== '0.6em') {
+      textdiv.style.fontSize = '1em';
+    }
+     
+    setTimeout(zoomtext, 1000);
+}
+
